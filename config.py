24f30ab856b72c8baa2ead0ff267ddab8d5aa81a
ashinwz <john.wang.jw6@roche.com>
2021-06-17 12:25:19 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+class Test_parameters:
+    host = '10.86.176.29'
+    port = 8817
--- demo.py
+++ demo.py
@@ -1,25 +1,42 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+import config
 
 app = dash.Dash(__name__)
 
-host = "10.86.176.29"
-port = 8817
+host = config.Test_parameters.host
+port = config.Test_parameters.port
+
+
+fig = px.scatter(x=range(10), y=range(10), height=400)
+fig.update_layout(clickmode='event+select')  # Config the click mode
 
 app.layout = html.Div(
     [
-        html.H1('下拉选择'),
+        html.H1('SELECT OPTIONES'),
         html.Br(),
         dcc.Dropdown(
+            id='option',
             options=[
-                {'label': '选项一', 'value': 1},
-                {'label': '选项二', 'value': 2},
-                {'label': '选项三', 'value': 3}
-            ]
-        )
+                {'label': 'One', 'value': 1},
+                {'label': 'Two', 'value': 2},
+                {'label': 'Three', 'value': 3}
+            ],
+            value='Please select options above!!!'
+        ),
+        html.P(id='select'),
+        dcc.Graph(figure=fig,id='scatter')
     ]
 )
+
+@app.callback(Output('select','children'),
+              Input('option','value'))
+def option_input(option):
+    return option
+
 
 if __name__ == '__main__':
     app.run_server(host=host,port=port)
