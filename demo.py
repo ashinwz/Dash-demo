5f5a77a8bbf6da4b5ed6b432f876b7ce4e19d471
ashinwz <john.wang.jw6@roche.com>
2021-06-17 13:00:28 +0800

    Add css and dbc

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
+# @ __auther__:    John Wang
+# @ __date__:      2021-06-17
+# @ __usage__:     Demo for Dash study
+
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import config
 
@@ -17,6 +22,9 @@ fig.update_layout(clickmode='event+select')  # Config the click mode
 app.layout = html.Div(
     [
         html.H1('SELECT OPTIONES'),
+        dbc.Alert(
+            "Hello，dash_bootstrap_components！", color='success'
+        ),
         html.Br(),
         dcc.Dropdown(
             id='option',
@@ -27,15 +35,29 @@ app.layout = html.Div(
             ],
             value='Please select options above!!!'
         ),
-        html.P(id='select'),
-        dcc.Graph(figure=fig,id='scatter')
+        html.P(id='select_option'),
+        dcc.Graph(figure=fig,id='scatter'),
+        html.Div([
+            'Selected Content：',
+            html.P(id='select')
+        ]),
     ]
 )
 
-@app.callback(Output('select','children'),
+
+# DEMO 01 :
+# Select options
+@app.callback(Output('select_option','children'),
               Input('option','value'))
 def option_input(option):
     return option
+
+# DEMO 02 :
+# Relayout data
+@app.callback(Output('select','children'),
+              Input('scatter','relayoutData'))
+def listen_to_select(relayoutData):
+    return str(relayoutData)
 
 
 if __name__ == '__main__':
