d18cc2ec8d48e29a7ba2edad12fe3fe3bb1f8f94
ashinwz <john.wang.jw6@roche.com>
2021-06-17 15:11:33 +0800

    add database connection

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Test_parameters:
     host = '10.86.176.29'
     port = 8817
+    DB_URI = 'mysql+pymysql://root:123456@10.86.176.29:3306/demo'
--- demo.py
+++ demo.py
@@ -5,16 +5,20 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, ClientsideFunction, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import config
+from sqlalchemy import create_engine
+import pandas as pd
 
 app = dash.Dash(__name__)
 
 host = config.Test_parameters.host
 port = config.Test_parameters.port
+DB   = config.Test_parameters.DB_URI
 
+engine = create_engine(DB,encoding="utf-8")
 
 fig = px.scatter(x=range(10), y=range(10), height=400)
 fig.update_layout(clickmode='event+select')  # Config the click mode
@@ -23,7 +27,7 @@ app.layout = html.Div(
     [
         html.H1('SELECT OPTIONES'),
         dbc.Alert(
-            "Hello，dash_bootstrap_components！", color='success'
+            "Hello，dash_bootstrap_components", color='success'
         ),
         html.Br(),
         dcc.Dropdown(
@@ -41,6 +45,57 @@ app.layout = html.Div(
             'Selected Content：',
             html.P(id='select')
         ]),
+        dbc.Container(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.Input(placeholder='Enter Username')),
+                        dbc.Col(dbc.Input(placeholder='Enter Password'))
+                    ]
+                )
+            ]
+        ),
+        html.Br(),
+        dcc.Checklist(
+            options=[
+                    {'label': 'Test1', 'value': 'Test1'},
+                    {'label': 'Test2', 'value': 'Test2'},
+                    {'label': 'Test3', 'value': 'Test3'},
+                    {'label': 'Test3', 'value': 'Test3'},
+            ],
+            value=['Test1']
+        ),
+
+        html.Br(),
+        dcc.RangeSlider(
+            min=0,
+            max=20,
+            step=0.5,
+            value=[5, 15]
+        ),
+        dbc.Container(
+            [
+                dbc.Label(id='show_message'),
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.Button('UpdateDB', id='refresh-db', style={'width': '100%'}), width=2),
+                        dbc.Col(dcc.Dropdown(id='db-table-names', placeholder='Select Table', style={'width': '100%'}), width=4),
+                        dbc.Col(dbc.Button('Query', id='query', style={'width': '100%'}), width=1)
+                    ]
+                ),
+                html.Hr(),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            id='query-result'
+                        )
+                    ]
+                )
+            ],
+            style={
+                'margin-top': '50px'
+            }
+        )
     ]
 )
 
@@ -60,5 +115,34 @@ def listen_to_select(relayoutData):
     return str(relayoutData)
 
 
+@app.callback(
+    Output('db-table-names', 'options'),
+    Output('show_message','children'),
+    Input('refresh-db', 'n_clicks'),
+    prevent_initial_call=True
+)
+def refresh_table_names(n_clicks):
+        table_names = pd.read_sql_query('select table_name from information_schema.tables where table_schema="demo"', con=engine)
+        if table_names.empty == False:
+            return [{'label': name, 'value': name} for name in table_names['TABLE_NAME']] ,'success'
+        else:
+            return [{'label': '', 'value': ''}], 'No data'
+
+@app.callback(
+    Output('query-result', 'children'),
+    Input('query', 'n_clicks'),
+    State('db-table-names', 'value'),
+    prevent_initial_call=True
+)
+def query_data_records(n_clicks, value):
+    if value:
+        # Retrive the data limit to 500 rows
+        query_result = pd.read_sql_query(f'select * from {value} limit 500', con=engine)
+        #print(query_result)
+        return html.Div(dbc.Table.from_dataframe(query_result, striped=True), style={'height': '600px', 'overflow': 'auto'})
+    else:
+        return dash.no_update
+
+
 if __name__ == '__main__':
-    app.run_server(host=host,port=port)
+    app.run_server(host=host,port=port,debug=True)
